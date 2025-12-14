a2260784f670a386d48e5220e079910cbb6df98a
ou <vremenno@bk.ru>
2025-12-14 20:56:01 +0300

    Оформил сайт автозапчастей

---
+++ script.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById('modal');
+const openBtn = document.getElementById('open-modal-btn');
+const closeBtn = document.getElementById('close-modal-btn');
+
+openBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
