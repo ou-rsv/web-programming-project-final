7d4a3a94d478d7290b86543979bb97555ad5af23
ou <vremenno@bk.ru>
2025-12-16 21:39:02 +0300

    React

--- webpack.config.js
+++ webpack.config.js
@@ -32,4 +32,20 @@ module.exports = {
     open: true,
     port: 3000,
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+
 };
